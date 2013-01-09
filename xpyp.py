bd99cbaab7a674d8fdb877466363c9aa64c82875
Mattias Andrée <maandree@operamail.com>
2013-01-09 11:59:05 +0100

    remove lines when possible + parse options
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- xpyp.py
+++ xpyp.py
@@ -23,21 +23,23 @@ import sys
 from subprocess import Popen, PIPE
 
 
-def print(text = '', end = '\n'):
-    sys.stdout.buffer.write((str(text) + end).encode('utf-8'))
-    sys.stdout.buffer.flush()
+def printerr(text = '', end = '\n'):
+    sys.stderr.buffer.write((str(text) + end).encode('utf-8'))
+    sys.stderr.buffer.flush()
 
 
 class xpyp:
     def __init__(self, remove_comments      = True, remove_blank_lines = True,
                        remove_documentation = True, remove_spaces      = True,
-                       compress_indention   = True, parse_escapes      = True):
+                       compress_indention   = True, parse_escapes      = True,
+                       remove_lines         = True):
         self.removeComments = remove_comments
         self.removeBlankLines = remove_blank_lines
         self.removeDocumentation = remove_documentation
         self.removeSpaces = remove_spaces
         self.compressIndention = compress_indention
         self.parseEscapes = parse_escapes
+        self.removeLines = remove_lines
     
     
     
@@ -58,6 +60,7 @@ class xpyp:
         if self.removeBlankLines    : self.packBlankLines   (script)
         if self.compressIndention   : self.packIndention    (script)
         if self.parseEscapes        : self.packEscapes      (script)
+        if self.removeLines         : self.packLines        (script)
         
         script = '\n'.join(script) + '\n'
         with open(scriptfile, 'wb'):
@@ -88,16 +91,46 @@ class xpyp:
     
     def packEscapes(self, code):
         pass
+    
+    
+    def packLines(self, code):
+        pass
 
 
 
 if __name__ == '__main__':
-    scriptfile = sys.argv[1]
-    
-    packer = xpyp() # TODO options
-    encoding = packer.getEncoding(scriptfile)
-    if encoding != None:
-        packer.pack(scriptfile, encoding)
-    else:
-        print('Cannot not compress file')
+    scriptfiles = []
+    dashed = False
+    c = False
+    d = False
+    s = False
+    b = False
+    i = False
+    e = False
+    l = False
+    for arg in sys.argv[1:]:
+        if dashed:
+            scriptfiles.append(arg)
+        elif arg == '--':
+            dashed = True
+        elif arg == '-c' : c = True
+        elif arg == '-d' : d = True
+        elif arg == '-s' : s = True
+        elif arg == '-b' : b = True
+        elif arg == '-i' : i = True
+        elif arg == '-e' : e = True
+        elif arg == '-l' : l = True
+        elif not arg.startsWith('-'):
+            scriptfiles.append(arg)
+        else:
+            printerr('Unrecognised option: ' + arg)
+    packer = xpyp()
+    if c or d or s or b or i or e or l:
+        packer = xpyp(c, d, s, b, i, e, l)
+    for scriptfile in scriptfiles:
+        encoding = packer.getEncoding(scriptfile)
+        if encoding != None:
+            packer.pack(scriptfile, encoding)
+        else:
+            printerr('Cannot not compress file')
 
